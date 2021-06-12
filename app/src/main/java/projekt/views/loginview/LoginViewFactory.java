0a4db74a081396ed83a75b95512d194e8f184fff
Szumi <patryk.szumilo324@gmail.com>
2021-06-12 18:38:00 +0200

    second commit

--- app/src/main/java/projekt/views/loginview/LoginViewFactory.java
+++
@@ -1,9 +0,0 @@
-package projekt.loginview;
-
-import javafx.stage.Stage;
-
-public class LoginViewFactory {
-    public LoginView create(LoginViewModel viewModel, Stage stage) {
-        return new LoginView(viewModel, stage);
-    }
-}

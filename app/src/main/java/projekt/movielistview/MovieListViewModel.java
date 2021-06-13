c2595dfe6c611ed8ae7a545ec6002d93582520b0
Szumi <patryk.szumilo324@gmail.com>
2021-06-13 21:28:32 +0200

    movielistmodule tests

--- app/src/main/java/projekt/movielistview/MovieListViewModel.java
+++ app/src/main/java/projekt/movielistview/MovieListViewModel.java
@@ -42,7 +42,7 @@ public class MovieListViewModel {
         readMovies();
 
 
-        cannotAddPosition=titleArea.isNull().or(directorArea.isNull()).or(hallArea.isNull()).or(dateArea.isNull());
+        cannotAddPosition=titleArea.isEmpty().or(directorArea.isEmpty()).or(hallArea.isEmpty()).or(dateArea.isEmpty());
 
 
 
@@ -51,7 +51,7 @@ public class MovieListViewModel {
 
     }
 
-    public StringProperty getAreaProperty() {
+    public StringProperty getTitleArea() {
         return titleArea;
     }
     public StringProperty getDirectorArea() {
